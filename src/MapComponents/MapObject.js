3b20af5d16f37a9372ec86badb82b78eab950336
dor1me <99968745+dor1me@users.noreply.github.com>
2024-09-07 21:02:15 +0300

    added states for mapObject component

--- src/MapComponents/MapObject.js
+++ src/MapComponents/MapObject.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const MapObject = () => {
+const MapObject = ({object_name}) => {
+    const[hover, setHover] = useState(false);
+    const HoverImage = `./public/source/object_images/${object_name}_outline.png`;
+    const UnhoverImage = `./public/source/object_images/${object_name}.png`;
+    const mapObjectImage = hover ? HoverImage : UnhoverImage;
 
+    return (
+        <div
+        className={'map-object'}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        >
+            <img src = {mapObjectImage}/>
+        </div>
+    )
 }
 
 export default MapObject;
