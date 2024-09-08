802a8e75922cd316d46c752c9ac762295985186d
dor1me <99968745+dor1me@users.noreply.github.com>
2024-09-08 17:21:18 +0300

    tried to change states of images

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import TeamCarousel from "./TeamComponents/TeamCarousel";
 import UnityGame from "./Games/Game_1";
 import MediaPlayer from "./MediaPlayerCompinents/MediaPlayer";
 import Parallax from "./MainComponents/Parallax";
+import MapObject from "./MapComponents/MapObject";
 
 function App() {
   return (
     <div className="App">
       <MainScreen/>
+      <MapObject/>
       {/*<UnityGame/>*/}
     </div>
   );
--- src/MapComponents/MapObject.js
+++ src/MapComponents/MapObject.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
 
-const MapObject = ({object_name}) => {
+const MapObject = (/*{object_name}*/) => {
     const[hover, setHover] = useState(false);
-    const HoverImage = `./public/source/object_images/${object_name}_outline.png`;
-    const UnhoverImage = `./public/source/object_images/${object_name}.png`;
+    // const HoverImage = `./public/source/object_images/${object_name}_outline.png`;
+    // const UnhoverImage = `./public/source/object_images/${object_name}.png`;
+    const HoverImage = `../../public/source/object_images/obed_outline.jpg`;
+    const UnhoverImage = `../../public/source/object_images/obed.jpg`;
     const mapObjectImage = hover ? HoverImage : UnhoverImage;
 
     return (
@@ -13,6 +15,9 @@ const MapObject = ({object_name}) => {
         onMouseLeave={() => setHover(false)}
         >
             <img src = {mapObjectImage}/>
+            <img src = '../../public/source/object_images/obed.jpg'/>
+            <img src={`${window.location.origin}/public/source/object_images/obed.jpg`} />
+
         </div>
     )
 }
