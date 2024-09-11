1e29f0be52ac8831ad8f536046cdd2d6f04c473d
dor1me <99968745+dor1me@users.noreply.github.com>
2024-09-11 11:56:20 +0300

    made header witchout contacts

--- src/MainComponents/Header.js
+++ src/MainComponents/Header.js
@@ -4,40 +4,44 @@ import {Logo} from './Logo'
 
 const Header = () => {
     return (
-        <header className={'header'}>
+        <div className={'header-container'}>
+            <header className={'header'}>
             <span className={'logo-list'}>
                 <img src = './source/logos/artek-logo.png' className={'artek-logo'}/>
                 <img src = './source/logos/vertical-stick.png' className={'stick-logo'}/>
                 <img src = './source/logos/edu-logo.png' className={'edu-logo'}/>
             </span>
-            <ul className={'header-list'}>
-                <li>
-                    <a>
-                        <span>Сведения об образовательной организации</span>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                        <span>Об "Артеке"</span>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                        <span>Жизнь "Артека"</span>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                        <span>Информация для родителей</span>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                        <span>Новости</span>
-                    </a>
-                </li>
-            </ul>
+                <ul className={'header-list'}>
+                    <li>
+                        <a>
+                            <span>Сведения об образовательной организации</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <span>Об "Артеке"</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <span>Жизнь "Артека"</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <span>Информация для родителей</span>
+                        </a>
+                    </li>
+                    <li className={'news-item'}>
+                            <span>Новости</span>
+                            <span><img src = './source/logos/news-icon.png' className={'news-icon'}/></span>
+                    </li>
+                </ul>
+                <span className={'contacts'}>
+                    <img src = './source/logos/contacts.png'/>
+                </span>
             </header>
+        </div>
     )
 }
 
