28413e5669cee5e8816b4ddcfcd615877c180f90
dor1me <99968745+dor1me@users.noreply.github.com>
2024-09-11 12:42:48 +0300

    made contact block

--- src/MainComponents/HeaderContacts.js
+++ src/MainComponents/HeaderContacts.js
@@ -7,14 +7,20 @@ const HeaderContacts = () => {
                 <img src = './source/icons/search-icon.png' className={'search-icon'}/>
                 <img src = './source/icons/blind-icon.png' className={'blind-icon'}/>
             </span>
-            <span  className={'phone-number'}>
-                <img src = './source/icons/search-icon.png' className={'search-icon'}/>
-                <span className={'number'}>8 800 600 20 85</span>
-                <span className={'number-info'}>Единый контактный центр</span>
-            </span>
-            <span  className={'email'}>
-                <img src = './source/icons/mail-icon.png' className={'mail-icon'}/>
+            <span className={'phone-and-mail'}>
+                <span className={'phone-number'}>
+                    <img src = './source/icons/phone-icon.png' className={'phone-icon'}/>
+                    <span>
+                        <span className={'number'}>8 800 600 20 85</span><br/>
+                        <span className={'number-info'}>Единый контактный центр</span>
+                    </span>
+                </span>
+                <span className={'email'}>
+                    <span>
+                      <img src = './source/icons/mail-icon.png' className={'mail-icon'}/>
+                    </span>
                 <span className={'mail'}>center@artek.org</span>
+            </span>
             </span>
 
         </span>
